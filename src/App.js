5408831b84a22972ef592111673d47941cb924c8
Federico Arona <federico.arona.94@gmail.com>
2021-07-17 21:47:59 +0200

    Add private route component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import PrivateRoute from './components/PrivateRoute'
+
+import Login from './pages/Login'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <BrowserRouter>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <PrivateRoute path="/dashboard"></PrivateRoute>
+        {/* 
+          <PrivateRoute path="/onlyAuthorizedAllowedHere/" component={MyComponent} />
+        */}
+        
+      </BrowserRouter>
     </div>
   );
 }
