8e9676c2190fcae0d161c07cddd480eb36913163
Federico Arona <federico.arona.94@gmail.com>
2021-07-20 01:53:30 +0200

    Make user request dynamic

--- src/services/Api/WatchlistApi.js
+++ src/services/Api/WatchlistApi.js
@@ -1,14 +1,19 @@
+import Cookies from 'universal-cookie';
 import { get, post, put, destroy } from "../../utlis/Api";
 
+const cookies = new Cookies();    
+const userId = cookies.get('user') !== undefined ? cookies.get('user').user.id : null;
+
 const Watchlist = {
+    
     get: () =>
-      get('/users/1/watchlist'),
+      get('/users/'+userId+'/watchlist'),
     startScraping: () =>
       post('/watchlist/scrape'),
     sendEans: (params) =>
-      post('/users/1/watchlist', params),
+      post('/users/'+userId+'/watchlist', params),
     clear: (params) =>
-      post('users/1/watchlist/actions/clear', params),
+      post('users/'+userId+'/watchlist/actions/clear', params),
       
 }
 
